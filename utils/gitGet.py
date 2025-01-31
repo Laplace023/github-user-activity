7fb75ea424c3efa06f059d7202f35444a261a482
Laplace023 <genesiscabaltica.work@gmail.com>
2025-02-01 02:10:11 +0800

    just preparing stuff

---
+++ utils/gitGet.py
@@ -0,0 +1,31 @@
+#my get module for the project
+
+def gitGet(argument):
+    print(argument)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    gitGet(str(sys.argv[1]))
+#making it usable for testing
