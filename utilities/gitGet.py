ee4e0ee8a6c453459c4d98236fc578b709ed5968
Laplace023 <genesiscabaltica.work@gmail.com>
2025-02-05 23:32:51 +0800

    added a utility to make a file

--- utilities/gitGet.py
+++ utilities/gitGet.py
@@ -1,18 +1,27 @@
 #my get module for the project
 import os
+import json
 
 
 def gitGet(argument, password):
-    print(argument)
+    print(f"Fetching the event logs for {argument}...")
     
     #the api call, I still have to filter the events
-    os.system(f"""
-    curl -L \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer {password}" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    https://api.github.com/users/{argument}/events
-    """)
+    try:
+        output = os.system(f"""
+        curl -L \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer {password}" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/users/{argument}/events \
+        >>tempData.json
+        """)
+    #this writes the output to temp.json
+    except:
+        print("something is wrong")
+
+    #writting it as json. I'll write it as a temporary json file
+
 
 
 
